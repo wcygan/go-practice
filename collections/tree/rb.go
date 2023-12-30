8a829a8859ca0e51704eef4a9a8ff2c94f7dc112
Will Cygan <wcygan.io@gmail.com>
2023-12-30 10:45:36 -0600

    rb tree
    seems like infinite loop somewhere
    
    fixed

---
+++ collections/tree/rb.go
@@ -0,0 +1,376 @@
+package tree
+
+import (
+	"cmp"
+)
+
+// RedBlackTree is a red-black tree data structure.
+// Its implementation is taken from 'Introduction to Algorithms' by Cormen et al.
+// It does not allow duplicate keys.
+type RedBlackTree[K cmp.Ordered, V any] struct {
+	root *RedBlackNode[K, V]
+	nil  *RedBlackNode[K, V]
+	size int
+}
+
+type RedBlackNode[K cmp.Ordered, V any] struct {
+	key    K
+	value  V
+	parent *RedBlackNode[K, V]
+	left   *RedBlackNode[K, V]
+	right  *RedBlackNode[K, V]
+	color  bool
+}
+
+const (
+	RED   = true
+	BLACK = false
+)
+
+func NewRedBlackTree[K cmp.Ordered, V any]() *RedBlackTree[K, V] {
+	nilNode := &RedBlackNode[K, V]{color: BLACK}
+
+	return &RedBlackTree[K, V]{
+		nil:  nilNode,
+		root: nilNode,
+	}
+}
+
+func (t *RedBlackTree[K, V]) Size() int {
+	return t.size
+}
+
+func (t *RedBlackTree[K, V]) IsEmpty() bool {
+	return t.Size() == 0
+}
+
+func (t *RedBlackTree[K, V]) Minimum() (*K, *V) {
+	minimum := t.root.minimum(t.nil)
+
+	if minimum == nil {
+		return nil, nil
+	}
+
+	return &minimum.key, &minimum.value
+}
+
+func (n *RedBlackNode[K, V]) minimum(nilNode *RedBlackNode[K, V]) *RedBlackNode[K, V] {
+	if n == nilNode {
+		return nil
+	}
+
+	x := n
+	for x.left != nilNode {
+		x = x.left
+	}
+
+	return x
+}
+
+func (t *RedBlackTree[K, V]) Maximum() (*K, *V) {
+	maximum := t.root.maximum(t.nil)
+
+	if maximum == nil {
+		return nil, nil
+	}
+
+	return &maximum.key, &maximum.value
+}
+
+func (n *RedBlackNode[K, V]) maximum(nilNode *RedBlackNode[K, V]) *RedBlackNode[K, V] {
+	if n == nilNode {
+		return nil
+	}
+
+	x := n
+	for x.right != nilNode {
+		x = x.right
+	}
+
+	return x
+}
+
+func (t *RedBlackTree[K, V]) Height() int {
+	return t.root.height(t.nil)
+}
+
+func (n *RedBlackNode[K, V]) height(nilNode *RedBlackNode[K, V]) int {
+	if n == nilNode {
+		return 0
+	}
+
+	leftHeight := n.left.height(nilNode)
+	rightHeight := n.right.height(nilNode)
+
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+
+	return rightHeight + 1
+}
+
+func (t *RedBlackTree[K, V]) Search(key K) (*V, bool) {
+	if t.root == nil {
+		// Empty tree
+		return nil, false
+	}
+
+	// Traverse the tree
+	x := t.root
+	for x != nil {
+		if key == x.key {
+			// Match is found
+			return &x.value, true
+		} else if key < x.key {
+			// Search left subtree
+			x = x.left
+		} else {
+			// Search right subtree
+			x = x.right
+		}
+	}
+
+	// Not found
+	return nil, false
+}
+
+func (t *RedBlackTree[K, V]) Insert(key K, value V) bool {
+	z := &RedBlackNode[K, V]{
+		key:   key,
+		value: value,
+		left:  t.nil,
+		right: t.nil,
+		color: RED,
+	}
+	y := t.nil
+	x := t.root
+
+	for x != t.nil {
+		y = x
+		if z.key == x.key {
+			// Duplicate key, ignore it
+			return false
+		} else if z.key < x.key {
+			x = x.left
+		} else {
+			x = x.right
+		}
+	}
+
+	z.parent = y
+	if y == t.nil {
+		t.root = z
+	} else if z.key < y.key {
+		y.left = z
+	} else {
+		y.right = z
+	}
+
+	t.insertFixup(z)
+	t.size++
+	return true
+}
+
+func (t *RedBlackTree[K, V]) insertFixup(z *RedBlackNode[K, V]) {
+	var y *RedBlackNode[K, V]
+	for z.parent.color == RED {
+		if z.parent == z.parent.parent.left {
+			y = z.parent.parent.right
+			if y.color == RED {
+				z.parent.color = BLACK
+				y.color = BLACK
+				z.parent.parent.color = RED
+				z = z.parent.parent
+			} else {
+				if z == z.parent.right {
+					z = z.parent
+					t.rotateLeft(z)
+				}
+				z.parent.color = BLACK
+				z.parent.parent.color = RED
+				t.rotateRight(z.parent.parent)
+			}
+		} else {
+			y = z.parent.parent.left
+			if y.color == RED {
+				z.parent.color = BLACK
+				y.color = BLACK
+				z.parent.parent.color = RED
+				z = z.parent.parent
+			} else {
+				if z == z.parent.left {
+					z = z.parent
+					t.rotateRight(z)
+				}
+				z.parent.color = BLACK
+				z.parent.parent.color = RED
+				t.rotateLeft(z.parent.parent)
+			}
+		}
+	}
+
+	t.root.color = BLACK
+}
+
+func (t *RedBlackTree[K, V]) rotateLeft(x *RedBlackNode[K, V]) {
+	y := x.right
+	x.right = y.left
+	if y.left != t.nil {
+		y.left.parent = x
+	}
+	y.parent = x.parent
+	if x.parent == t.nil {
+		t.root = y
+	} else if x == x.parent.left {
+		x.parent.left = y
+	} else {
+		x.parent.right = y
+	}
+	y.left = x
+	x.parent = y
+}
+
+func (t *RedBlackTree[K, V]) rotateRight(x *RedBlackNode[K, V]) {
+	y := x.left
+	x.left = y.right
+	if y.right != t.nil {
+		y.right.parent = x
+	}
+	y.parent = x.parent
+	if x.parent == t.nil {
+		t.root = y
+	} else if x == x.parent.right {
+		x.parent.right = y
+	} else {
+		x.parent.left = y
+	}
+	y.right = x
+	x.parent = y
+}
+
+func (t *RedBlackTree[K, V]) Delete(key K) bool {
+	// Find the node to delete
+	z := t.root
+	for z != t.nil {
+		if key == z.key {
+			break
+		} else if key < z.key {
+			z = z.left
+		} else {
+			z = z.right
+		}
+	}
+
+	if z == t.nil {
+		// Not found
+		return false
+	}
+
+	// Delete the node
+	t.deleteNode(z)
+	t.size--
+	return true
+}
+
+func (t *RedBlackTree[K, V]) deleteNode(z *RedBlackNode[K, V]) {
+	y := z
+	yOriginalColor := y.color
+	var x *RedBlackNode[K, V]
+
+	if z.left == t.nil {
+		x = z.right
+		t.transplant(z, z.right)
+	} else if z.right == t.nil {
+		x = z.left
+		t.transplant(z, z.left)
+	} else {
+		y = z.right.minimum(t.nil)
+		yOriginalColor = y.color
+		x = y.right
+
+		if y.parent == z {
+			x.parent = y
+		} else {
+			t.transplant(y, y.right)
+			y.right = z.right
+			y.right.parent = y
+		}
+
+		t.transplant(z, y)
+		y.left = z.left
+		y.left.parent = y
+		y.color = z.color
+	}
+
+	if yOriginalColor == BLACK {
+		t.deleteFixup(x)
+	}
+}
+
+func (t *RedBlackTree[K, V]) deleteFixup(x *RedBlackNode[K, V]) {
+	for x != t.root && x.color == BLACK {
+		if x == x.parent.left {
+			w := x.parent.right
+			if w.color == RED {
+				w.color = BLACK
+				x.parent.color = RED
+				t.rotateLeft(x.parent)
+				w = x.parent.right
+			}
+
+			if w.left.color == BLACK && w.right.color == BLACK {
+				w.color = RED
+				x = x.parent
+			} else if w.right.color == BLACK {
+				w.left.color = BLACK
+				w.color = RED
+				t.rotateRight(w)
+				w = x.parent.right
+			}
+
+			w.color = x.parent.color
+			x.parent.color = BLACK
+			w.right.color = BLACK
+			t.rotateLeft(x.parent)
+			x = t.root
+		} else {
+			w := x.parent.left
+			if w.color == RED {
+				w.color = BLACK
+				x.parent.color = RED
+				t.rotateRight(x.parent)
+				w = x.parent.left
+			}
+
+			if w.right.color == BLACK && w.left.color == BLACK {
+				w.color = RED
+				x = x.parent
+			} else if w.left.color == BLACK {
+				w.right.color = BLACK
+				w.color = RED
+				t.rotateLeft(w)
+				w = x.parent.left
+			}
+
+			w.color = x.parent.color
+			x.parent.color = BLACK
+			w.left.color = BLACK
+			t.rotateRight(x.parent)
+			x = t.root
+		}
+	}
+
+	x.color = BLACK
+}
+
+func (t *RedBlackTree[K, V]) transplant(u, v *RedBlackNode[K, V]) {
+	if u.parent == t.nil {
+		t.root = v
+	} else if u == u.parent.left {
+		u.parent.left = v
+	} else {
+		u.parent.right = v
+	}
+
+	v.parent = u.parent
+}
---
+++ collections/tree/rb_test.go
@@ -0,0 +1,190 @@
+package tree
+
+import (
+	"math"
+	"testing"
+	"testing/quick"
+)
+
+func TestNewRedBlackTree(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	if tree == nil {
+		t.Error("NewRedBlackTree() should not return nil")
+	}
+}
+
+func TestRedBlackTree_Insert(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	if tree.Size() != 1 {
+		t.Errorf("Size() = %v, want %v", tree.Size(), 1)
+	}
+}
+
+func TestRedBlackTree_IgnoreDuplicateKeys(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	tree.Insert(1, 200)
+	if tree.Size() != 1 {
+		t.Errorf("Size() = %v, want %v", tree.Size(), 1)
+	}
+	value, found := tree.Search(1)
+	if !found || *value != 100 {
+		t.Errorf("Search() = %v, %v, want %v, %v", *value, found, 100, true)
+	}
+}
+
+func TestRedBlackTree_InsertSamePair(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	for i := 0; i < 100; i++ {
+		tree.Insert(1, 100)
+	}
+	if tree.Size() != 1 {
+		t.Errorf("Size() = %v, want %v", tree.Size(), 1)
+	}
+}
+
+func TestRedBlackTree_Search(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	value, found := tree.Search(1)
+	if !found || *value != 100 {
+		t.Errorf("Search() = %v, %v, want %v, %v", *value, found, 100, true)
+	}
+}
+
+func TestRedBlackTree_Delete(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	tree.Delete(1)
+	if tree.Size() != 0 {
+		t.Errorf("Size() = %v, want %v", tree.Size(), 0)
+	}
+}
+
+func TestRedBlackTree_IsEmpty(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	if !tree.IsEmpty() {
+		t.Errorf("IsEmpty() = %v, want %v", tree.IsEmpty(), true)
+	}
+}
+
+func TestRedBlackTree_Minimum(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(5, 500)
+	tree.Insert(3, 300)
+	tree.Insert(1, 100)
+	tree.Insert(4, 400)
+	tree.Insert(2, 200)
+	minKey, minValue := tree.Minimum()
+	if *minKey != 1 || *minValue != 100 {
+		t.Errorf("Minimum() = %v, %v, want %v, %v", *minKey, *minValue, 1, 100)
+	}
+}
+
+func TestRedBlackTree_Maximum(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(5, 500)
+	tree.Insert(3, 300)
+	tree.Insert(1, 100)
+	tree.Insert(4, 400)
+	tree.Insert(2, 200)
+	maxKey, maxValue := tree.Maximum()
+	if *maxKey != 5 || *maxValue != 500 {
+		t.Errorf("Maximum() = %v, %v, want %v, %v", *maxKey, *maxValue, 5, 500)
+	}
+}
+
+func TestRedBlackTree_InsertNonExistentKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	inserted := tree.Insert(1, 100)
+	if !inserted || tree.Size() != 1 {
+		t.Errorf("Insert() = %v, Size() = %v, want %v, %v", inserted, tree.Size(), true, 1)
+	}
+}
+
+func TestRedBlackTree_InsertExistingKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	inserted := tree.Insert(1, 200)
+	if inserted || tree.Size() != 1 {
+		t.Errorf("Insert() = %v, Size() = %v, want %v, %v", inserted, tree.Size(), false, 1)
+	}
+}
+
+func TestRedBlackTree_SearchNonExistentKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	_, found := tree.Search(1)
+	if found {
+		t.Errorf("Search() = %v, want %v", found, false)
+	}
+}
+
+func TestRedBlackTree_SearchExistingKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	_, found := tree.Search(1)
+	if !found {
+		t.Errorf("Search() = %v, want %v", found, true)
+	}
+}
+
+func TestRedBlackTree_DeleteNonExistentKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	deleted := tree.Delete(1)
+	if deleted {
+		t.Errorf("Delete() = %v, want %v", deleted, false)
+	}
+}
+
+func TestRedBlackTree_DeleteExistingKey(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	deleted := tree.Delete(1)
+	if !deleted || tree.Size() != 0 {
+		t.Errorf("Delete() = %v, Size() = %v, want %v, %v", deleted, tree.Size(), true, 0)
+	}
+}
+
+func TestRedBlackTree_HeightOfEmptyTree(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	if height := tree.Height(); height != 0 {
+		t.Errorf("Height() = %v, want %v", height, 0)
+	}
+}
+
+func TestRedBlackTree_HeightOfNonEmptyTree(t *testing.T) {
+	tree := NewRedBlackTree[int, int]()
+	tree.Insert(1, 100)
+	if height := tree.Height(); height != 1 {
+		t.Errorf("Height() = %v, want %v", height, 1)
+	}
+}
+
+func TestRedBlackTree_Height(t *testing.T) {
+	// Define a function to be tested with random inputs
+	// Using uint8 to use small numbers for testing
+	testHeight := func(n uint8) bool {
+		// Warning: Sometimes this fails when n = 0xff (255)
+		tree := NewRedBlackTree[int, int]()
+
+		// Insert n nodes
+		for i := uint8(0); i < n; i++ {
+			tree.Insert(int(i), int(i))
+		}
+
+		// Check the height
+		height := tree.Height()
+
+		// The height of a Red-Black Tree with n nodes is always less than or equal to 2*log2(n+1)
+		// This is a property of Red-Black Trees
+		maxHeight := 2 * int(math.Log2(float64(n+1)))
+
+		return height <= maxHeight
+	}
+
+	// Run the quick check test
+	if err := quick.Check(testHeight, &quick.Config{MaxCount: 50}); err != nil {
+		t.Error(err)
+	}
+}
